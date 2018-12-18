22eeb70b587551e0221cbeaf243dba21045d37cb
Alan32Liu <alan32.liu@gmail.com>
2018-12-18 11:59:22 +1100

    adding QS as a new backend

--- claripy/__init__.py
+++ claripy/__init__.py
@@ -66,7 +66,8 @@ def BV(name, size, explicit_name=None): #pylint:disable=function-redefined
 from . import backend_manager as _backend_manager
 _backend_manager.backends._register_backend(_backends_module.BackendConcrete(), 'concrete', True, True)
 _backend_manager.backends._register_backend(_backends_module.BackendVSA(), 'vsa', False, False)
-
+_backend_manager.backends._register_backend(_backends_module.BackendZ3QuickSampler(), 'QS', False,
+                                            False)
 if not os.environ.get('WORKER', False) and os.environ.get('REMOTE', False):
     try:
         _backend_z3 = _backends_module.backendremote.BackendRemote()
